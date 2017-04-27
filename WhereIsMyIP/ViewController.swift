456691c2597ac0febe90728ad07933db0ca0c6a2
Gai lowvapong <mumu@gais-mac.local>
2017-04-27 09:12:18 -0700

    Initial Commit

---
+++ WhereIsMyIP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereIsMyIP
+//
+//  Created by gai lowvapong on 4/27/17.
+//  Copyright © 2017 gai lowvapong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
